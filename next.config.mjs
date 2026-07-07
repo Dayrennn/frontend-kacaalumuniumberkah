e66d004fca79d84e368b627afbfe1f99ddeef30a
Dayrennn <raflyramandha789@gmail.com>
2026-07-07 22:54:20 +0700

    lagi dan lagi

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://backend-kacaalumuniumberkah.vercel.app/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
